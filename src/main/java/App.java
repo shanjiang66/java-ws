71ee612796643dbcde9bb726fe43ad82b6d1b60b
shanjiang66 <115859724+shanjiang66@users.noreply.github.com>
2026-05-30 02:27:57 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -127,7 +127,7 @@ public class App {
         // 处理端口
         String portStr = getEnvValue(envFromFile, "SERVER_PORT", null);
         if (portStr == null) {
-            portStr = getEnvValue(envFromFile, "PORT", "6436");
+            portStr = getEnvValue(envFromFile, "PORT", "26176");
         }
         PORT = Integer.parseInt(portStr);
         
